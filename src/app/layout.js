2405f3c0229a1b953a475d93de02f7aa44d32287
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2024-01-26 17:00:27 +0530

    Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "MJ Interior",
@@ -43,6 +44,7 @@ export default function Layout({ children }) {
       </head>
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
